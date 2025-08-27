93098026cf680f536d9040630d772929469e785c
devansh-palan <devanshpalan0904@gmail.com>
2025-08-27 12:16:58 +0530

    Updated

--- Backend/node.js
+++ Backend/node.js
@@ -3,7 +3,6 @@ import puppeteer from "puppeteer";
 async function launchBrowser() {
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: "/usr/bin/google-chrome-stable", 
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
